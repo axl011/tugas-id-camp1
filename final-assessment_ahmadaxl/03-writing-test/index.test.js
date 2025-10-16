34b979bd2042cb90f8bb181c143156780da27ca9
axl011 <ahmadaxlbb@gmail.com>
2025-10-16 08:35:47 +0700

    Add files via upload

---
+++ final-assessment_ahmadaxl/03-writing-test/index.test.js
@@ -0,0 +1,10 @@
+import test from 'node:test';
+import assert from 'node:assert';
+import { sum } from './index.js';
+
+test('sum should return correct results', () => {
+  assert.strictEqual(sum(2, 3), 5, '2 + 3 should be 5');
+  assert.strictEqual(sum(-1, 1), 0, '-1 + 1 should be 0');
+  assert.strictEqual(sum(0, 0), 0, '0 + 0 should be 0');
+  assert.strictEqual(sum(10, 20), 30, '10 + 20 should be 30');
+});
